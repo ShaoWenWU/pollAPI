7f2cb04fcf1b29a54865cc8e1e6ef272f95c4f3b
Shao-Wen Wu <wu8149@gmail.com>
2020-06-19 19:38:32 +0200

    added Swagger for API documentation

--- src/main/java/com/shaowen/pollAPI/controller/PollController.java
+++ src/main/java/com/shaowen/pollAPI/controller/PollController.java
@@ -2,6 +2,8 @@ package com.shaowen.pollAPI.controller;
 
 import com.shaowen.pollAPI.domain.Poll;
 import com.shaowen.pollAPI.service.PollService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/polls")
+@Api(tags = { "Poll Controller" })
 public class PollController {
 
   @Autowired
@@ -22,24 +25,28 @@ public class PollController {
 
   // List all data in the database
   @GetMapping(path = "/list")
+  @ApiOperation(value = "Poll list", response = Poll.class, tags = "Poll Controller")
   public Iterable<Poll> list() {
     return pollService.list();
   }
 
   // Query title
   @GetMapping(path = "/title={_title}")
+  @ApiOperation(value = "Find all polls by title", response = Poll.class, tags = "Poll Controller")
   public List<Poll> findByTitle(@PathVariable("_title") String _title) {
     return pollService.findByTitleContaining(_title);
   }
 
   // Query user name
   @GetMapping(path = "/user={_name}")
+  @ApiOperation(value = "Find all polls created by user name", response = Poll.class, tags = "Poll Controller")
   public List<Poll> findByName(@PathVariable("_name") String _name) {
     return pollService.findByInitiator_NameContaining(_name);
   }
 
   // Query after initiated date
   @GetMapping(path = "/dateAfter={_initiated}")
+  @ApiOperation(value = "Find all polls created after a certain date", response = Poll.class, tags = "Poll Controller")
   public List<Poll> findByName(@PathVariable("_initiated") Long _initiated) {
     return pollService.findByInitiatedAfter(_initiated);
   }
